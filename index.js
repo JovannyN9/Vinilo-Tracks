6f5a753ccbb4853eafa8c940140edb407b67d819
Jovanny Garcia <jgarciahdz1993@gmail.com>
2024-11-18 11:14:28 -0800

    Public folder added and Hero section Finished

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import bodyParser from "body-parser"
+
+const app = express ();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+
+app.get("/", (req, res) => {
+    res.render ("home");
+     });
+
+     app.listen (port, () => {
+        console.log (`Server running on port ${port}`)
+     });
